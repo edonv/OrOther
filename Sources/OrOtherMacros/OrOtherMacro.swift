0bb27c5017c31f55789ddd69e0101e15577a69bb
Edon <edon@valdman.works>
2024-02-22 16:44:33 -0600

    Update OrOtherMacro.swift
    Working implementation of new macro.

--- Sources/OrOtherMacros/OrOtherMacro.swift
+++ Sources/OrOtherMacros/OrOtherMacro.swift
@@ -3,31 +3,155 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
+// MARK: - MemberMacro
+public struct OrOtherMacro: MemberMacro {
     public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
+        of node: AttributeSyntax,
+        providingMembersOf declaration: some DeclGroupSyntax,
+        conformingTo protocols: [TypeSyntax],
         in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
+    ) throws -> [DeclSyntax] {
+        // Make sure it's attached to an enum
+        guard declaration.is(EnumDeclSyntax.self) else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresEnum.diagnose(at: declaration))
+            return []
         }
+        
+        // Parse the first nested member as an enum named Options
+        guard let optionEnumDecl = declaration.memberBlock.members.compactMap({ $0.decl.as(EnumDeclSyntax.self) }).first else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresOptionsEnum.diagnose(at: declaration))
+            return []
+        }
+        
+        // Make sure it's a private enum
+        guard optionEnumDecl.modifiers.contains(where: \.isPrivateAccessLevelModifier) else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresOptionsPrivateEnum.diagnose(at: declaration))
+            return []
+        }
+        
+        // Extract 1 generic type from enum,
+        guard let genericClause = node.attributeName.as(IdentifierTypeSyntax.self)?.genericArgumentClause,
+              genericClause.arguments.count == 1,
+              let rawType = genericClause.arguments.first?.argument else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresEnum1RawType.diagnose(at: node))
+            return []
+        }
+        
+        // RawValue type from Options enum,
+        guard let optionsRawType = optionEnumDecl.inheritanceClause?.inheritedTypes.first?.type else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresOptionsEnumRawType.diagnose(at: node))
+            return []
+        }
+        
+        // ...and make sure they match
+        guard case .identifier(let genericTypeName) = rawType.as(IdentifierTypeSyntax.self)?.name.tokenKind,
+              case .identifier(let optionsRawTypeName) = optionsRawType.as(IdentifierTypeSyntax.self)?.name.tokenKind,
+              genericTypeName == optionsRawTypeName else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresRawTypesMatch.diagnose(at: node))
+            return []
+        }
+        
+        // Make sure it has at least one case
+        let optionEnumCaseElements: [EnumCaseElementSyntax] = optionEnumDecl.memberBlock.members
+            .compactMap { member in
+                member.decl.as(EnumCaseDeclSyntax.self)?.elements
+            }
+            .flatMap { $0 }
+        guard !optionEnumCaseElements.isEmpty else {
+            context.diagnose(OrOtherMacroDiagnostic.requiresOptionsNonEmptyEnum.diagnose(at: declaration))
+            return []
+        }
+        
+        // Get access control keyword from primary declaration
+        let access = declaration.modifiers.first(where: \.isAccessLevelModifier)
 
-        return "(\(argument), \(literal: argument.description))"
+        // typealias
+        let rawValueTypeAliasDecl: DeclSyntax = "\(access)typealias RawValue = \(rawType)"
+        
+        // cases
+        let casesDecl = EnumCaseDeclSyntax {
+            for enumCase in optionEnumCaseElements {
+                EnumCaseElementSyntax(name: enumCase.name)
+            }
+            
+            EnumCaseElementSyntax(
+                name: .identifier("other"),
+                parameterClause: .init(parameters: [.init(type: rawType.trimmed)])
+            )
+        }.as(DeclSyntax.self)
+        
+        // rawValue
+        let rawValuePropertyDecl = VariableDeclSyntax(
+            modifiers: .init([access].compactMap { $0 }),
+            bindingSpecifier: .keyword(.var)
+        ) {
+            PatternBindingListSyntax {
+                PatternBindingSyntax(
+                    pattern: IdentifierPatternSyntax(identifier: .identifier("rawValue")),
+                    typeAnnotation: TypeAnnotationSyntax(type: IdentifierTypeSyntax(name: .identifier("RawValue"))),
+                    accessorBlock: AccessorBlockSyntax(
+                        accessors: .getter(CodeBlockItemListSyntax {
+                            CodeBlockItemSyntax(
+                                item: .expr(try! SwitchExprSyntax("switch self") {
+                                    for enumCase in optionEnumCaseElements {
+                                        SwitchCaseSyntax("case .\(enumCase.name): return Options.\(enumCase.name).rawValue")
+                                    }
+                                    
+                                    SwitchCaseSyntax("case .other(let string): return string")
+                                }.as(ExprSyntax.self)!)
+                            )
+                        })
+                    )
+                )
+            }
+        }.as(DeclSyntax.self)
+        
+        // init
+        let initDecl = try! InitializerDeclSyntax("\(access)init(rawValue: RawValue)") {
+            try! IfExprSyntax("if let this = Options(rawValue: rawValue)") {
+                try! SwitchExprSyntax("switch this") {
+                    for enumCase in optionEnumCaseElements {
+                        SwitchCaseSyntax("case .\(enumCase.name): self = .\(enumCase.name)")
+                    }
+                }
+            } else: {
+                "self = .other(rawValue)"
+            }
+        }.as(DeclSyntax.self)
+        
+        return [
+            rawValueTypeAliasDecl,
+            casesDecl,
+            rawValuePropertyDecl,
+            initDecl
+        ].compactMap { $0 }
+    }
+}
+
+// MARK: - ExtensionMacro
+
+extension OrOtherMacro: ExtensionMacro {
+    public static func expansion(
+        of attribute: AttributeSyntax,
+        attachedTo declaration: some DeclGroupSyntax,
+        providingExtensionsOf type: some TypeSyntaxProtocol,
+        conformingTo protocols: [TypeSyntax],
+        in context: some MacroExpansionContext
+    ) throws -> [ExtensionDeclSyntax] {
+        var extensions = [DeclSyntax]()
+        
+        // MARK: RawRepresentable
+        if !protocols.contains(where: { $0.as(InheritedTypeSyntax.self)?.type == "RawRepresentable" }) {
+            extensions.append("extension \(type.trimmed): RawRepresentable {}")
+        }
+        
+        return extensions.map { $0.cast(ExtensionDeclSyntax.self) }
     }
 }
 
 @main
 struct OrOtherPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
+        OrOtherMacro.self,
     ]
 }
