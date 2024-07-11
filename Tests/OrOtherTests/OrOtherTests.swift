0898dc9fe87f6715016e0e9fa5e95987253fb227
Edon <edon@valdman.works>
2024-07-11 18:18:06 -0500

    Update OrOtherTests.swift

--- Tests/OrOtherTests/OrOtherTests.swift
+++ Tests/OrOtherTests/OrOtherTests.swift
@@ -5,6 +5,7 @@ import XCTest
 // Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
 #if canImport(OrOtherMacros)
 import OrOtherMacros
+import OrOther
 
 let testMacros: [String: Macro.Type] = [
     "stringify": StringifyMacro.self,
