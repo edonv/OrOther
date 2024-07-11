433267e1b7f6d8564c34d71519a99af0344f4e39
Edon <edon@valdman.works>
2024-07-11 18:20:51 -0500

    Update OrOtherTests.swift
    Updated testing.

--- Tests/OrOtherTests/OrOtherTests.swift
+++ Tests/OrOtherTests/OrOtherTests.swift
@@ -7,41 +7,33 @@ import XCTest
 import OrOtherMacros
 import OrOther
 
-let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
-]
-#endif
-
 final class OrOtherTests: XCTestCase {
-    func testMacro() throws {
-        #if canImport(OrOtherMacros)
-        assertMacroExpansion(
-            """
-            #stringify(a + b)
-            """,
-            expandedSource: """
-            (a + b, "a + b")
-            """,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
+    @OrOther
+    private enum EnumTest: RawRepresentable, Codable, CaseIterable {
+        static let allCases: [Self] = [.a, .b, .c, .d, .e, .f]
+        
+        private enum Options: String {
+            case a
+            case b
+            case c, d, e, f
+        }
     }
-
-    func testMacroWithStringLiteral() throws {
-        #if canImport(OrOtherMacros)
-        assertMacroExpansion(
-            #"""
-            #stringify("Hello, \(name)")
-            """#,
-            expandedSource: #"""
-            ("Hello, \(name)", #""Hello, \(name)""#)
-            """#,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
+    
+    private struct StructTest: Codable {
+        let option: EnumTest
+        let string: String
+    }
+    
+    func testMacro() throws {
+        let array: [EnumTest] = [.a, .b, .d, .f, .other("YES")]
+        let data = try JSONEncoder().encode(array)
+        print("array:", String(data: data, encoding: .utf8))
+        
+        let test = StructTest(option: .c, string: "STRING")
+        let dataTest = try JSONEncoder().encode(test)
+        print("struct:", String(data: dataTest, encoding: .utf8))
+        let decoded = try JSONDecoder().decode(StructTest.self, from: dataTest)
+        print("struct decoded:", decoded.option)
     }
 }
+#endif
